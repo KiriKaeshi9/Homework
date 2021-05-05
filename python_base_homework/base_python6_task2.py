0bbad8d40cc1af2b6ea1ccab1fdb18af576a34e6
unknown <xom9l4ok30@gmail.cpm>
2021-05-05 14:59:02 +0300

    First Commit

--- python_base_homework/base_python6_task2.py
+++ python_base_homework/base_python6_task2.py
@@ -1,34 +1,40 @@
 # Создайте программу, которая эмулирует работу сервиса по сокращению ссылок. Должна быть
 # реализована возможность ввода изначальной ссылки и короткого названия и получения изначальной
 # ссылки по её названию.
+dict_of_links = dict()
 
 
-def create_links():
-    links = dict()
-    while True:
-        long_link = input('Enter some link: ')
-        name_link = input('Enter name of the link: ')
-        if long_link == '' or name_link == '':
-            break
-        links[name_link] = long_link
-    return links
+def create_link(name_link):
+    link = dict()
+    long_link = input('Enter some link: ')
+    link[name_link] = long_link
+    return link
 
 
-def get_links(links):
-    while True:
+def get_link(links):
+    choose_link = input('Enter the link name: ')
+    return links[choose_link]
+
+
+while True:
+    action = input('Enter your action with a links(a=add, g=get or press "Enter" to exit): ')
+    if action == 'a':
+        new_link = input('Enter the name of new link: ')
+        if new_link not in dict_of_links.keys() and new_link != '':
+            dict_of_links.update(create_link(new_link))
+            print()
+        else:
+            print('This link exists.\n')
+    elif action == 'g':
         try:
-            choose_link = input('Enter the link name: ')
-            if choose_link == '':
-                break
-            print(links[choose_link])
+            exist_link = get_link(dict_of_links)
+            if exist_link == '':
+                raise KeyError
+            else:
+                print(exist_link, '\n')
         except KeyError:
-            print('Wrong the link name. Try again')
-            continue
-
-
-dict_of_links = create_links()
-print(dict_of_links)
-get_links(dict_of_links)
-
-
-
+            print('The link does not exist.\n')
+    elif action == '':
+        break
+    else:
+        print('Choose right action.\n')
